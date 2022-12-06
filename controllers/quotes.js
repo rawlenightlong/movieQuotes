103bb7533b9875a0ac5d2ede9515dc21ecb6fd72
Rawle Becerril <rawlesxm@gmail.com>
2022-12-05 22:36:22 -0500

    redirect

--- controllers/quotes.js
+++ controllers/quotes.js
@@ -1,20 +1,33 @@
+const { Router } = require('express')
 const express = require ('express')
 const Quote = require('../models/quotes') 
 
 const router = express.Router()
 
 // LANDING ROUTE
-// router.get('/', (req, res) => {
-//     Quote.find({})
-//     .then((quotes) => {
-//         res.render('quotes/index.ejs', {quotes})
-//     })
-// })
-
 router.get('/', (req, res) => {
     res.redirect('/quotes')
 })
 
+// INDEX ROUTE
+router.get('/quotes', (req, res) => {
+    Quote.find({})
+    .then((quotes) => {
+        res.render('quotes/index.ejs', {quotes})
+    })
+})
+
+// NEW ROUTE
+
+// DESTROY ROUTE
+
+// UPDATE ROUTE
+
+// CREATE ROUTE
+
+// EDIT ROUTE
+
+// SHOW ROUTE
 
 
 
